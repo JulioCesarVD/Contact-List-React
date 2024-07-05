21185e1ca2251720088c794f843ed17a561db5e9
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-07-05 10:39:19 +0000

    add layouts for the navigation bar and footer

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<div className="container">
+			<nav className="navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<span className="navbar-brand mb-0 h1">AGENDA</span>
 				</Link>
-			</div>
-		</nav>
+				<div className="ml-auto">
+					<Link to="/AddNewContact">
+						<button className="btn btn-primary" onClick={() => actions.handleEditContact(false)}>Agrega un nuevo contacto</button>
+					</Link>
+				</div>
+			</nav>
+
+		</div>
 	);
 };
