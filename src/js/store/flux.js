1556b78cacd00faa8b965ca84dc867b25cc11e8e
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-07-05 10:41:57 +0000

    Added state management and CRUD actions for contacts

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,110 @@
-const getState = ({ getStore, getActions, setStore }) => {
+
+
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [],
+			editContact: false,
+			selectedContact: {},
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			fetchPostAgenda: () => { //create agenda
+				fetch('https://playground.4geeks.com/contact/agendas/JulioCesarVd', {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => response.json())
+					.then(data => {
+						console.log(data);
+					})
+					.catch(error => console.log(error))
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			fetchPostContact: (name, phone, email, address) => {
+				const newContact = {
+					name,
+					phone,
+					email,
+					address,
+				}
+				fetch('https://playground.4geeks.com/contact/agendas/JulioCesarVD/contacts', {
+					method: "POST",
+					body: JSON.stringify(newContact),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => response.json())
+					.then(data => {
+						console.log(data);
+					})
+					.catch(error => console.log(error))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			fetchGetContact: () => { 
+				fetch('https://playground.4geeks.com/contact/agendas/JulioCesarVD/contacts', {
+					method: "GET",
+				})
+					.then(response => response.json())
+					.then(data => {
+						console.log(data) 
+						const store = getStore(); 
+						setStore({ contacts: data.contacts });
+					})
+					.catch(error => console.log(error))
+			},
+			fetchDeleteContact: (id) => { 
+				const deleteContact = {
+					slug: "JulioCesarVD",
+					contact_id: id
+				}
+				fetch(`https://playground.4geeks.com/contact/agendas/JulioCesarVD/contacts/${id}`, {
+					method: "DELETE",
+					body: JSON.stringify(deleteContact),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => {
+						console.log(response);
+						if (response.ok) {
+							getActions().fetchGetContact();
+						};
+					})
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+					.catch(error => console.log(error))
+			},
+			fetchUpdateContact: (name, phone, email, address) => {
+				const updateContact = {
+					name,
+					phone,
+					email,
+					address,
+				}
+				fetch(`https://playground.4geeks.com/contact/agendas/JulioCesarVD/contacts/${getStore().contactId}`, {
+					method: "PUT",
+					body: JSON.stringify(updateContact),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => response.json())
+					.then(data => {
+						console.log(data);
+						getActions().fetchGetContact();
+					})
+					.catch(error => console.log(error))
+			},
+			handleEditContact: (value, id) => {
+				setStore({ editContact: value, contactId: id })
+			},
+			selectedContact: (name, phone, email, address) => {
+				setStore({ selectedContact: { name: name, phone: phone, email: email, address: address } })
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			},
+			
 		}
 	};
-};
+}
 
 export default getState;
